d5e0c0fe96e44dc47908b0006406fbc8f0472974
Giovanni Lampong <blazevanni@gmail.com>
2018-11-02 15:45:42 -0400

    Just finished VM2.5 Data Binding on Youtube

--- src/app/components/user/login/login.component.ts
+++ src/app/components/user/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from "../../../services/user.service.client";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -8,10 +10,22 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
   username: string;
   password: string;
+  errorFlag: boolean;
 
-  constructor() { }
+  constructor(private userService: UserService, private router: Router) { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  login() {
+    const user = this.userService.findUserByCredentials(
+      this.username,
+      this.password
+      );
+
+      if (user) {
+        this.router.navigate(["user", user._id]); // "user/" + ._id
+      } else {
+        this.errorFlag = true;
+      }
   }
-
 }
--- src/app/services/user.service.client.ts
+++ src/app/services/user.service.client.ts
@@ -27,7 +27,7 @@ users = [
 
   createUser(user) {
 
-    user._id = Math.random().toString;
+    user._id = Math.random().toString();
 
     this.users.push(user);
 
